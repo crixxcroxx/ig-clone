d8907921b6db85d83d8bc0bc50c8727a7d11eb31
crixxcroxx <cristajesawa@gmail.com>
2021-12-16 14:05:09 +0800

    Add modal on click

--- src/components/Img/Img.jsx
+++ src/components/Img/Img.jsx
@@ -1,12 +1,46 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
 import { AiFillHeart } from "react-icons/ai";
 import { IoChatbubbleSharp } from "react-icons/io5";
 import getRandomInt from "../../utils/getRandomInt";
 import "./img.scss";
 
+Modal.setAppElement(document.getElementById("root"));
+
 export default function Img(props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function openModal() {
+    setIsModalOpen(true);
+  }
+
+  function closeModal() {
+    setIsModalOpen(false);
+  }
+
+
   return (
-    <div className="img-container">
+    <>
+
+    <Modal
+      isOpen={isModalOpen}
+      onRequestClose={closeModal}
+      className="post-img-modal"
+      overlayClassName="post-img-modal-overlay"
+      contentLabel="Image Modal"
+    >
+    <div className="modal-content">
+      <img src={props.src} alt="modal-img" />
+      <div className="post-info">
+        asasasas
+      </div>
+    </div>
+    </Modal>
+
+    <div className="img-container" onClick={openModal}>
       <img src={props.src} alt="user-img" />
+
       {props.styledHover && (
         <div className="img-hover">
           <div className="img-hover-inf">
@@ -20,5 +54,7 @@ export default function Img(props) {
         </div>
       )}
     </div>
+
+    </>
   );
 }
